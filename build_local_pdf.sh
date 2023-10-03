90c0f0c82330bc936eefa728a46e825177c53d21
JackH <jack@realtimeradio>
2023-10-03 17:13:52 +0100

    Add references
    And slightly tweak template, to prevent "cslreferences not defined" error

--- build_local_pdf.sh
+++ build_local_pdf.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-pandoc docs/*.md -o rcf_design.pdf -s -V colorlinks -V links-as-notes --number-sections --template template.latex
+pandoc docs/*.md -o rcf_design.pdf -s -V colorlinks -V links-as-notes --number-sections --template template-pd2.9.latex --bibliography bibliography.bib
